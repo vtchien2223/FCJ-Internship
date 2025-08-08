a86d177d1823951f2e7db8857dd60b69da32596a
Vũ Tiến Chiến <161411928+vtchien2223@users.noreply.github.com>
2025-08-08 16:30:28 +0700

    Delete WebApplication directory

--- WebApplication/BE/models/Major.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const majorSchema = new mongoose.Schema({
-  majorId: { type: String, required: true, unique: true }, // mã chuyên ngành
-  name: { type: String, required: true }, // tên chuyên ngành
-  description: { type: String }, // mô tả
-});
-
-module.exports = mongoose.model('Major', majorSchema); 
--- WebApplication/FE/src/student/index.js
+++
@@ -1,6 +0,0 @@
-export { default as StudentProfile } from './StudentProfile';
-export { default as ScoreView } from './ScoreView';
-export { default as ScheduleView } from './ScheduleView';
-export { default as ImportStudents } from './ImportStudents';
-export { default as MainContent } from './MainContent';
-export { default as Sidebar } from './Sidebar'; 
